e01447a8956cc503803296cefcc5e19eae8776c7
jahskee <jahskee@yahoo.com>
2018-06-01 17:14:09 -0400

    update

--- components/api/api.js
+++ components/api/api.js
@@ -1,8 +1,8 @@
 /*jshint esversion: 6 */
 API = {};
-
+const token = `?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphaHNrZWVAeWFob28uY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTI3Mjg1NjMzLCJleHAiOjMxNTU1MjcyODU2MzN9.wQHAoNW9CtbWckhQmWM7Q2k-UV0SwT-zifkRrvgWo98`
 API.getCategories = async () => {
-  response = await fetch("https://www.stratteos.us/api/category");
+  response = await fetch(`https://www.stratteos.us/api/category?${token}`);
   if (response.status != 200)
     throw `Server api error reponse.status=${response.status}`;
   categories = await response.json();
@@ -10,7 +10,7 @@ API.getCategories = async () => {
 };
 
 API.getQuestions = async category => {
-  response = await fetch(`https://www.stratteos.us/api/questions/${category}`);
+  response = await fetch(`https://www.stratteos.us/api/questions/${category}?${token}`);
   if (response.status != 200)
     throw `Server api error reponse.status=${response.status}`;
   questions = await response.json();
